d0fb58ca4f4be423d412844758adf541e0022d4a
DOSSEH Shalom <shalom.dosseh@epitech.eu>
2023-07-31 20:44:41 +0100

    Testing github

--- GitHub-Graph/app.py
+++ GitHub-Graph/app.py
@@ -2,12 +2,11 @@ import os
 from random import randint
 
 for i in range(1, 365):
-
     for j in range(0, randint(1, 10)):
         d = str(i) + ' days ago'
         with open("file.txt", 'a') as file:
             file.write(d)
         os.system("git add .")
-        os.system("git commit --date=" + d + '" -m "Testing github"')
+        os.system('git commit --date="' + d + '" -m "Testing github"')
 
 os.system('git push -u origin main')
